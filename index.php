7f960bdbae73ab92b020dedf763512f785d43ce2
kevincha98 <kevinchackoabraham2023b@mca.ajce.in>
2023-02-27 21:13:07 +0530

    Fivth xommit

--- index.php
+++ index.php
@@ -47,6 +47,19 @@ $db = mysqli_connect('localhost', 'root', '', 'care');
     <link rel="stylesheet" href="css/slick.css">
     <!-- style CSS -->
     <link rel="stylesheet" href="css/style.css">
+
+    <script type="text/javascript">
+    (function(d, m){
+        var kommunicateSettings = 
+            {"appId":"1184c9013f5377226f54b79f86a5bca1f","popupWidget":true,"automaticChatOpenOnNavigation":true};
+        var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
+        s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+        var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
+        window.kommunicate = m; m._globals = kommunicateSettings;
+    })(document, window.kommunicate || {});
+/* NOTE : Use web server to view HTML files as real-time update will not work if you directly open the HTML file in the browser. */
+</script>
+
 </head>
 
 <body>
